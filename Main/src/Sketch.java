3365be3f30464d0cb7c18232cd4994ea4a24b0b9
bayewq <bayewq@gmail.com>
2023-06-12 13:39:03 -0400

    hello

---
+++ Main/src/Sketch.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+  public void settings() {
+    size(400, 400);
+  }
+
+  public void setup() {
+    background(204, 255, 255);
+  }
+
+  public void draw() {
+    
+  }
+
+  public void keyPressed() {
+    if (keyCode == LEFT) {
+    } else if (keyCode == RIGHT) {
+
+    }else if (keyCode == UP){
+
+    }else if (keyCode == DOWN){
+
+    }
+  }
+}
