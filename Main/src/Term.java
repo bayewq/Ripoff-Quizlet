95f3f8904d9e72c70e1f1d3516c141d48c80f887
Bay <bay@explorerhop.com>
2023-06-12 20:41:20 -0400

    apsinda

---
+++ Main/src/Term.java
@@ -0,0 +1,15 @@
+public class Term {
+    private String name;
+
+    public Term(String name){
+        this.name = name;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
